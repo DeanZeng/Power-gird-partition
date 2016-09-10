1dd2206cac33d074a16650503f19bf68d090efb5
DeanZeng <zfangdi@gmail.com>
2016-09-10 11:08:34 +0800

    Revert "方法"
    This reverts commit c00c6bd8524d7ba30c98bcfdb3293411386e9df9.

--- bus39.m
+++ bus39.m
@@ -76,7 +76,7 @@ mpc.version = '2';
 
 %%-----  Power Flow Data  -----%%
 %% system MVA base
-mpc.baseMVA = 200;
+mpc.baseMVA = 100;
 
 %% bus data
 %	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
